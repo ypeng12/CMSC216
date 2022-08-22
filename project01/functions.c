0f97d883718495e054b12bbe89d783e69100c788
ypeng12 <111715993+ypeng12@users.noreply.github.com>
2022-08-21 21:47:06 -0400

    Add files via upload

---
+++ project01/functions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* (c) Larry Herman, 2022.  You are allowed to use this code yourself, but
+   not to provide it to anyone else. */
+
+/* illustrates treating an array name as a pointer, and applying pointer
+   arithmetic operations to it */
+
+int main(void) {
+
+  char sh1[] = "elephant";
+  char *p; 
+  p = strchr(strchr(str,'e')+1 , 'e ');
+  printf("%d\n")
+  return 0;
+}
---
+++ project01/functions.h
@@ -0,0 +1,9 @@
+/* (c) Larry Herman, 2022.  You are allowed to use this code yourself, but
+ * not to provide it to anyone else. */
+
+/* DO NOT MODIFY THIS FILE OR YOUR CODE WILL NOT COMPILE ON THE SUBMIT
+ * SERVER. */
+
+int classify_triangle(int side1, int side2, int side3);
+long reverse_digits(long n);
+unsigned long catalan(short int n);
